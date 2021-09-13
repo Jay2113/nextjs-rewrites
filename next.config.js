15c47d72c734cd962a28412140256d0184032ac0
Raval <jayrava@38f9d3641d1b.ant.amazon.com>
2021-09-13 10:38:36 -0400

    Modified config

--- next.config.js
+++ next.config.js
@@ -1,3 +1,20 @@
 module.exports = {
   reactStrictMode: true,
+  trailingSlash: true,
+  async rewrites() {
+    return {
+      fallback: [
+        // These rewrites are checked after both pages/public files
+        // and dynamic routes are checked
+        {
+          source: '/:slug*/',
+          destination: 'https://production.d26rnk3qbcbxwm.amplifyapp.com/:slug*/',
+        },
+        {
+          source: '/:path*',
+          destination: 'https://production.d26rnk3qbcbxwm.amplifyapp.com/:path*',
+        },
+      ],
+    }
+  },
 }
